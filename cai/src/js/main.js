baff9f66b1d5f33e53f0c9b7a054fe47bfe02864
YoungH <chnhuangyan@gmail.com>
2018-08-16 01:52:40 -0400

    cai
    cai

---
+++ cai/src/js/main.js
@@ -0,0 +1,16 @@
+<script type="text/javascript" language="javascript">
+var nav = document.getElementById("menu");
+var links = nav.getElementsByTagName("li");
+var lilen = nav.getElementsByTagName("a");
+var currenturl = document.location.href;
+var last = 0;
+for (var i=0;i<links.length;i++)
+{
+  var linkurl =  lilen[i].getAttribute("href");
+  if(currenturl.indexOf(linkurl)!=-1)
+  {
+    last = i;
+  }
+}
+links[last].className = "menu_on";
+</script>
